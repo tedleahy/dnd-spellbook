b7f0fc82ea4dd7a549eb65ef5660986cb0bf1617
Ted Leahy <leahyted@proton.me>
2025-08-24 00:02:33 +0100

    Order spells by name, grouped by level when getting all spells via /spells

--- api/app/controllers/spells_controller.rb
+++ api/app/controllers/spells_controller.rb
@@ -1,6 +1,6 @@
 class SpellsController < ApplicationController
   def index
-    render json: Spell.all, except: %i[created_at updated_at]
+    render json: Spell.order(:level, :name), except: %i[created_at updated_at]
   end
 
   def show
